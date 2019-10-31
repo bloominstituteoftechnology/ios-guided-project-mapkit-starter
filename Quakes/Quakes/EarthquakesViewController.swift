ea6562e6bc5133ebc22acf38931483f9478101c3
Paul Solt <PaulSolt@gmail.com>
2019-10-31 12:07:32 -0400

    Starter project

---
+++ Quakes/Quakes/EarthquakesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EarthquakesViewController.swift
+//  iOS8-Quakes
+//
+//  Created by Paul Solt on 10/3/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class EarthquakesViewController: UIViewController {
+		
+//	@IBOutlet var mapView: MKMapView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+}
---
+++ Quakes/Quakes/QuakeDetailView.swift
@@ -0,0 +1,77 @@
+//
+//  QuakeDetailView.swift
+//  iOS6-Quakes
+//
+//  Created by Paul Solt on 7/11/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class QuakeDetailView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        latitudeLabel.setContentHuggingPriority(.defaultLow+1, for: .horizontal)
+        
+        let placeDateStackView = UIStackView(arrangedSubviews: [magnitudeLabel, dateLabel])
+        placeDateStackView.spacing = UIStackView.spacingUseSystem
+        let latLonStackView = UIStackView(arrangedSubviews: [latitudeLabel, longitudeLabel])
+        latLonStackView.spacing = UIStackView.spacingUseSystem
+        let mainStackView = UIStackView(arrangedSubviews: [placeDateStackView, latLonStackView])
+        mainStackView.axis = .vertical
+        mainStackView.spacing = UIStackView.spacingUseSystem
+        
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(mainStackView)
+        mainStackView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        mainStackView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        mainStackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        mainStackView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+    // MARK: - Private
+    
+    private func updateSubviews() {
+        guard let quake = quake else { return }
+        let magnitude = quake.mag
+        magnitudeLabel.text = String(magnitude) + " magnitude"
+        dateLabel.text = dateFormatter.string(from: quake.time)
+        latitudeLabel.text = "Lat: " + latLonFormatter.string(from: quake.coordinate.latitude as NSNumber)!
+        longitudeLabel.text = "Lon: " + latLonFormatter.string(from: quake.coordinate.longitude as NSNumber)!
+    }
+    
+    // MARK: - Properties
+    
+    var quake: Quake? {
+        didSet {
+            updateSubviews()
+        }
+    }
+    
+    private let magnitudeLabel = UILabel()
+    private let dateLabel = UILabel()
+    private let latitudeLabel = UILabel()
+    private let longitudeLabel = UILabel()
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let result = DateFormatter()
+        result.dateStyle = .short
+        result.timeStyle = .short
+        return result
+    }()
+    
+    private lazy var latLonFormatter: NumberFormatter = {
+        let result = NumberFormatter()
+        result.numberStyle = .decimal
+        result.minimumIntegerDigits = 1
+        result.minimumFractionDigits = 2
+        result.maximumFractionDigits = 2
+        return result
+    }()
+}
