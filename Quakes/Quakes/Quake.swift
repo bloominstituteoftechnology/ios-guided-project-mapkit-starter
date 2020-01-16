ee8105b2dc46c1b9aecfe290d66a7f2018ea45ea
Dan M <djmthedev@gmail.com>
2020-01-16 10:23:10 -0800

    added testing and functionality to decode a quake

--- Quakes/Quakes/Quake.swift
+++ Quakes/Quakes/Quake.swift
@@ -18,17 +18,36 @@ class Quake: NSObject, Decodable {
     // coordinate
     
     let magnitude: Double
+    let place: String
+    let time: Date
+    let longitude: Double
+    let latitude: Double
     
     enum QuakeCodingKeys: String, CodingKey {
         case magnitude = "mag"
         case properties
+        case place
+        case time
+        case longitude
+        case latitude
+        case geometry
+        case coordinates
     }
     
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: QuakeCodingKeys.self)
         let properties = try container.nestedContainer(keyedBy: QuakeCodingKeys.self, forKey: .properties)
+        let geometry = try container.nestedContainer(keyedBy: QuakeCodingKeys.self, forKey: .geometry)
+        var coordinates = try geometry.nestedUnkeyedContainer(forKey: .coordinates)
         
-        self.magnitude = 0
+        self.magnitude = try properties.decode(
+            Double.self, forKey: .magnitude)
+        self.place = try properties.decode(String.self, forKey: .place)
+        self.time = try properties.decode(Date.self, forKey: .time)
+        
+        self.longitude = try coordinates.decode(Double.self) // This mutates the contents of the container, it pulls stuff out
+        self.latitude = try coordinates.decode(Double.self)
+        
         
         super.init()
     }
--- Quakes/QuakesTests/QuakesTests.swift
+++ Quakes/QuakesTests/QuakesTests.swift
@@ -13,7 +13,27 @@ class QuakesTests: XCTestCase {
 
     func testQuakeJSONParsing() {
         // Red, Green, Refactor
-        // 
+        // red = failing test
+        // green = impement logic to pass test
+        // refactor = clean up code or reorganize
+        
+        
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .millisecondsSince1970
+        
+        do {
+            let quake = try decoder.decode(Quake.self, from: quakeData)
+            let date = Date(timeIntervalSince1970: 1388620296020.0 / 1000)
+            
+            // Test (Expected, Actual)
+            XCTAssertEqual(1.29, quake.magnitude, accuracy: 0.001)
+            XCTAssertEqual("10km SSW of Idyllwild, CA", quake.place)
+            XCTAssertEqual(date, quake.time)
+            XCTAssertEqual(-116.7776667, quake.longitude, accuracy: 0.001)
+            XCTAssertEqual(33.663333299999998, quake.latitude, accuracy: 0.001)
+        } catch {
+            XCTFail("Error decoding: \(error)")
+        }
         
         
     }
