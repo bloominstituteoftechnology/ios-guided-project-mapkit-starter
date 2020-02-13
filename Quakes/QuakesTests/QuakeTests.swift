bde2c6a0745bafd04793ec3f0fbc620a5847b005
Paul Solt <PaulSolt@gmail.com>
2020-02-13 08:32:19 -0500

    Updated QuakeTests.swift

---
+++ Quakes/QuakesTests/QuakeTests.swift
@@ -0,0 +1,19 @@
+//
+//  QuakeTests.swift
+//  QuakesTests
+//
+//  Created by Paul Solt on 10/31/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import XCTest
+@testable import Quakes
+
+class QuakeTests: XCTestCase {
+
+    // Use throws so you don't need do/catch
+    func testQuake() throws {
+        
+    }
+
+}
